3959f7632c4f3492755b68d59f6f41bd26c3dc91
cpbhai <harshvsingh.223@gmail.com>
2021-09-10 15:07:37 +0530

    Update 2.0

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  env: {
-    BASE_URL: "http://localhost:3000",
-    MONGODB_URL:
-      "mongodb+srv://hvs:thakur@cluster0.sjxbo.mongodb.net/yamuzazondb?retryWrites=true&w=majority",
-    ACCESS_TOKEN_SECRET:
-      "+ . DRIP 9 YELP apple JACK (  ~ } : walmart YELP 2 , walmart",
-    CLOUD_UPDATE_PRESET: "yamunazon",
-    CLOUD_NAME: "yamunazon",
-    CLOUD_API: "https://api.cloudinary.com/v1_1/yamunazon/image/upload",
-  },
-  images: {
-    domains: ["res.cloudinary.com"],
-  },
+	env: {
+		BASE_URL: "https://www.yamunazon.com",
+		MONGODB_URL:
+			"mongodb+srv://hvs:thakur@cluster0.sjxbo.mongodb.net/yamuzazondb?retryWrites=true&w=majority",
+		ACCESS_TOKEN_SECRET:
+			"+ . DRIP 9 YELP apple JACK (  ~ } : walmart YELP 2 , walmart",
+		CLOUD_UPDATE_PRESET: "yamunazon",
+		CLOUD_NAME: "yamunazon",
+		CLOUD_API: "https://api.cloudinary.com/v1_1/yamunazon/image/upload",
+	},
+	images: {
+		domains: ["res.cloudinary.com"],
+	},
 };
